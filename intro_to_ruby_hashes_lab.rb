e3bad33356ec79175e29195404464a9df5d5f9a9
voracious-accelerator-8615 <margo.reiss.gold@gmail.com>
2020-08-07 21:47:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,7 @@ end
 end
 
 def monopoly_with_third_tier
-  hash = base_hash
+  ashh = base_hash
   hash[:railroads][:rent_in_dollars] = {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}
   hash
 end
